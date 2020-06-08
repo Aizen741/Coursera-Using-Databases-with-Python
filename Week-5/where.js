e6b0ef2a168d2bfc1f566b44957e0fee52fd12be
Aizen741 <rahulsivadas2009@gmail.com>
2020-06-09 05:09:20 +0530

    Week-5

---
+++ Week-5/where.js
@@ -0,0 +1,11 @@
+myData = [
+[21.1702401,72.83106070000001, 'Surat, Gujarat, India'],
+[50.06688579999999,19.9136192, 'aleja Adama Mickiewicza 30, 30-059 Kraków, Poland'],
+[33.4242399,-111.9280527, 'Tempe, AZ 85281, USA'],
+[39.3301047,-82.1076022, 'Athens, OH 45701, USA'],
+[28.3588163,75.58802039999999, 'Vidya Vihar, Pilani, Rajasthan 333031, India'],
+[6.8939569,3.7187158, 'Ilishan-Remo, Nigeria'],
+[25.2677203,82.99125819999999, 'Ajagara, Varanasi, Uttar Pradesh 221005, India'],
+[12.9504351,77.5021998, 'Main Rd, Gnana Bharathi, Bengaluru, Karnataka 560056, India'],
+[31.5469132,-97.1210998, '1311 S 5th St, Waco, TX 76706, USA']
+];
